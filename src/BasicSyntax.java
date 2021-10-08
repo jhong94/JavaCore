e1166c7f42357b5a7d42eaeed3219ae250819ea5
jhong94 <jackyhong94@gmail.com>
2021-10-08 01:16:42 -0400

    Third Commit

--- src/BasicSyntax.java
+++ src/BasicSyntax.java
@@ -5,6 +5,21 @@ public class BasicSyntax {
      * With multiple lines
      */
     public static void main(String []args) {
-        System.out.println("This is Basic Syntax");
+        Student myStudent = new Student ();
+        myStudent.firstName = "Dwight";
+                myStudent.lastName = "Schrute";
+                        myStudent.gpa = 3.8;
+                        myStudent.major = "Business";
+                        myStudent.age = 24;
+                        myStudent.onProbation = false;
+        Student myStudent2 = new Student ();
+            myStudent2.firstName = "Jim";
+                myStudent2.lastName = "Halpert";
+                    myStudent2.gpa = 2.8;
+                        myStudent2.major = "Business";
+                            myStudent2.age = 24;
+                                myStudent2.onProbation = true;
+        System.out.println(myStudent.firstName);
+        System.out.println(myStudent.age);
     }
 }
