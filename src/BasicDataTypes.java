d1ba6e7453a012d59bfe182d358efb99834be893
jhong94 <jackyhong94@gmail.com>
2021-10-11 18:05:37 -0400

    Sixth Commit

---
+++ src/BasicDataTypes.java
@@ -0,0 +1,36 @@
+public class BasicDataTypes {
+    public static void main(String[] args){
+        byte myShinyMetal = -65;
+        short Lebowski = 3545;
+        int toTheThickOfIt = 453634564;
+        long TimeNoSee = 53587476976L;
+        float LikAButterFly = 3.3194f;
+        double down = 1.33;
+        boolean isPizzaDelicious = true;
+        char Mander ='u';
+        String YouAlong = "Never gonna give you up, never gonna let you down";
+        int decVal = 24;
+        int octVal = 007;
+        int hexVal = 0X25abcdef;
+        int binVal = 0B101010;
+            System.out.println(myShinyMetal);
+            System.out.println(Lebowski);
+            System.out.println(toTheThickOfIt);
+            System.out.println(TimeNoSee);
+            System.out.println(LikAButterFly);
+            System.out.println(down);
+            System.out.println(isPizzaDelicious);
+            System.out.println(Mander);
+            System.out.println(YouAlong);
+            System.out.println(decVal);
+            System.out.println(octVal);
+            System.out.println(hexVal);
+            System.out.println(binVal);
+
+
+
+
+
+
+    }
+}
